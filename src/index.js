9966f11af4ec75eb3425cb26ed6ff32b39b927be
Gian Ramirez <gecedeveloper@gmail.com>
2018-03-04 16:18:02 -0500

    agregando plugins dev y express

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component () {
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-	btn.innerHTML = 'Click me and check the console'
+	btn.innerHTML = 'HEllo worldsldlsdlldsllsd'
 	btn.onclick = printMe
 
 	element.appendChild(btn)
